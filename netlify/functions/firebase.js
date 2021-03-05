23628496275a34e040072c850f041a85239fa057
Alec-Atlas <alec.atlas@kellogg.northwestern.edu>
2021-03-04 21:06:28 -0600

    sdgadg

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {    
+apiKey: "AIzaSyDCv_amY1cFvZEnnkNyLlb8CRXbs9OShJo",
+authDomain: "kiei-451-1951a.firebaseapp.com",
+projectId: "kiei-451-1951a",
+storageBucket: "kiei-451-1951a.appspot.com",
+messagingSenderId: "809941901635",
+appId: "1:809941901635:web:fd732ecfd1a7f619ff0ff6"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
